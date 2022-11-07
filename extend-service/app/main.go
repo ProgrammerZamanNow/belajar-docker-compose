e7d543d6ead56b3311b710ab4942c4a7626d476c
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2022-11-07 17:33:36 +0700

    belajar docker compose

---
+++ extend-service/app/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	fmt.Println("Run app in port : " + port)
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+    mode := os.Getenv("MODE")
+    response := "Hello " + mode
+	fmt.Fprintf(w, response)
+}
